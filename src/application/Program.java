3afe68151226c4481d5a4e06b631f159e301a686
EvertonRussomanno <evertonmartins154@gmail.com>
2023-05-28 16:09:13 -0300

    Very bad solution last

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,13 +38,13 @@ public class Program {
 			Date now = new Date();
 			if(checkIn.before(now) || checkOut.before(now)) {
 				System.out.println("Error in reservation: Reservation dates for update must be future dates");
-			}else if (!checkOut.after(checkIn)) {
+			}
+			else if (!checkOut.after(checkIn)) {
 				System.out.println("Error in reservation: Check-out date must be after check-in date");
-			}else {
+			}
+			else {
 				reservation.updateDates(checkIn, checkOut);
 				System.out.println("Reservation: " + reservation);
-				
-				//teste git
 			}
 		}
 		
